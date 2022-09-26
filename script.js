8d223d730eddddb375b53d2a064b9b16b5b40f36
crlsrgz <c@c.com>
2022-09-26 13:02:49 +0200

    func: retrieve values fro the from is done, REexp to check url need to be better implemented

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function showModal() {
   websiteNameEl.focus();
 }
 
-// MOdal Event listener
+// Modal Event listener
 
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => {modal.classList.remove('show-modal')});
@@ -20,3 +20,42 @@ window.addEventListener('click', (event) => {
 // console.log(event.target);
 event.target === modal ? modal.classList.remove('show-modal'): false;
 });
+
+/*::::::::::::::::::::::::::
+    Validate Form 
+::::::::::::::::::::::::::::*/
+
+function validate(namevalue, urlValue) {
+  // 💡 Check this regular expression, ans see if it is understandable
+  // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+
+  if (urlValue.match(regex)) {
+    alert('match');
+  } 
+  if (!urlValue.match(regex)) {
+    alert('Please provide a valiu web address')
+    return false;
+  }
+}
+
+// Handle Data from From
+
+function storeBookmark(event) {
+  event.preventDefault();
+
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  // 💡 Includes accepts just one parameter 
+  if(!urlValue.includes('https://') && !urlValue.includes('http://')){
+    urlValue = `https://${urlValue}`
+  }
+  console.log(nameValue, urlValue);
+  validate(nameValue, urlValue)
+
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
